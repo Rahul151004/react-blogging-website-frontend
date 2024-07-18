4a2fd50b6bbdc4efef0afaa71151ffb56b332c52
Rahul <rbhat5480@gmail.com>
2024-07-18 21:16:49 +0530

    useCreateArticle updated

--- blogging-website/src/pages/Auth.jsx
+++ blogging-website/src/pages/Auth.jsx
@@ -16,7 +16,7 @@ function Auth() {
       // api request to login or register
 
       const { data } = await axios.post(
-        `http://localhost:3001/api/users${isRegister ? "" : "/login"}`,
+        `https://react-blogging-website-backend.onrender.com/api/users${isRegister ? "" : "/login"}`,
         { user: values }
       );
 
